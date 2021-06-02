0271533060113aa692ac4f9b8d4701c3c42dcec7
p-wtt <parksangmin.wyatt@gmail.com>
2021-06-02 18:48:14 +0900

    update uikit: add gradient button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
+export { default as GradientButton } from "./GradientButton";
 export { default as IconButton } from "./IconButton";
 export type { ButtonProps, Sizes as ButtonSizes, Variants as ButtonVariants } from "./types";
