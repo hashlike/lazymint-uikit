2ee75bed6c77ff6bdd979e86e1487639bcc23d3e
in-ho Kim <padosolee89@gmail.com>
2021-06-09 14:39:46 +0900

    .

--- src/widgets/Menu/Panel.tsx
+++ src/widgets/Menu/Panel.tsx
@@ -24,7 +24,7 @@ const StyledPanel = styled.div<{ isPushed: boolean; showMenu: boolean }>`
   height: 100vh;
   transition: padding-top 0.2s, width 0.2s;
   border-right: ${({ isPushed }) => (isPushed ? "2px solid rgba(133, 133, 133, 0.1)" : 0)};
-  z-index: 11;
+  z-index: 40;
   overflow: ${({ isPushed }) => (isPushed ? "initial" : "hidden")};
   transform: translate3d(0, 0, 0);
 
